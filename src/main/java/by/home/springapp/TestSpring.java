d8e0d6a6d4ac15774a17a562d429cead680446c6
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-12 16:52:08 +0300

    realised init method

--- src/main/java/by/home/springapp/TestSpring.java
+++ src/main/java/by/home/springapp/TestSpring.java
@@ -5,14 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassicalMusic classicalMusic = context.getBean("musicBean2",ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 
-
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        context.close();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
-        musicPlayer.playMusicList();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+//        context.close();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+//        musicPlayer.playMusicList();
 
     }
 
