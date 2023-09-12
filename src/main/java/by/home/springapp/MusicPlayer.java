889a445c69733b3a32c1ff9eea3f41a2ed694a64
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-12 15:41:01 +0300

    added setter to MusicPlayer

--- src/main/java/by/home/springapp/MusicPlayer.java
+++ src/main/java/by/home/springapp/MusicPlayer.java
@@ -3,11 +3,16 @@ package by.home.springapp;
 public class MusicPlayer {
     private Music music;
 
+
     //Inversion of controll
     public MusicPlayer(Music music) {
         this.music = music;
     }
     public void playMusic(){
         System.out.println("playing "+music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
     }
 }
