b02cf08688abd0e5a9171608a70d1bbb579b0395
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-12 15:32:59 +0300

    created MusicPlayer class

---
+++ src/main/java/by/home/springapp/MusicPlayer.java
@@ -0,0 +1,13 @@
+package by.home.springapp;
+
+public class MusicPlayer {
+    private Music music;
+
+    //Inversion of controll
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+    public void playMusic(){
+        System.out.println("playing "+music.getSong());
+    }
+}
