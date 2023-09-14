513a6bf41df5ce0f4a97f1c5d3ee19b61f1be7aa
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-14 10:46:07 +0300

    added new fields and constructor for them

--- src/main/java/by/home/springapp/MusicPlayer.java
+++ src/main/java/by/home/springapp/MusicPlayer.java
@@ -1,44 +1,28 @@
 package by.home.springapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MusicPlayer {
+    private ClassicalMusic classicalMusic;
+    private PopMusic popMusic;
 
-    private String name;
-    private int volume;
-    private Music music;
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic(){
-        System.out.println("Playing "+ music.getSong());
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, PopMusic popMusic) {
+        this.classicalMusic = classicalMusic;
+        this.popMusic = popMusic;
     }
 
 
+
+    public void playMusic() {
+        System.out.println("Playing " + classicalMusic.getSong());
+        System.out.println("Playing " + popMusic.getSong());
+    }
 
 
 }
